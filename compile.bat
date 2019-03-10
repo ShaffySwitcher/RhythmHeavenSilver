88b1a0393b7dbbfdf2a5ff701dc382f401af9ca3
ShaffyYoutube <thedave2cz@gmail.com>
2019-03-10 20:36:10 +0100

    Debug Menu Compile

--- compile.bat
+++ compile.bat
@@ -1,3 +1,5 @@
+REM Variables.
+set %debug%=0
 :compile
 del "Game\rh-atlus.gba"
 copy "Game\rh-jpn.gba" "Game\rh-atlus.gba"
@@ -93,5 +95,11 @@ rhcomp.exe Graphics/MusicSelect/CD62EC_musicselect_map.bin
 rhcomp.exe Graphics/MusicSelect/CD6434_musicselect_warning_map.bin
 armips.exe compile.asm -sym mysym.sym
 flips.exe --apply "Font Hack 4 - 1px spacer.ips" "Game\rh-eng.gba"
+if %debug% == 1 (
+  echo Debug Menu Activated
+  flips.exe --apply "Debug_Menu.ips" "Game\rh-eng.gba"
+) else (
+  echo Debug Menu Not Activated
+)
 pause
 goto compile
