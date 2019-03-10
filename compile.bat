06637694290d336985a77454cd5bdb9cbb026f80
ShaffyYoutube <thedave2cz@gmail.com>
2019-03-10 18:53:10 +0100

    WidthFonted Game is bad, right?

--- compile.bat
+++ compile.bat
@@ -92,5 +92,6 @@ rhcomp.exe Graphics/Drums/cc6ccc_drums_map.bin
 rhcomp.exe Graphics/MusicSelect/CD62EC_musicselect_map.bin
 rhcomp.exe Graphics/MusicSelect/CD6434_musicselect_warning_map.bin
 armips.exe compile.asm -sym mysym.sym
+flips.exe --apply "Font Hack 4 - 1px spacer.ips" "Game\rh-eng.gba"
 pause
 goto compile
