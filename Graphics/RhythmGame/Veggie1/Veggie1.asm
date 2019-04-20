0297bc4c3261636dfdd0db6b061a39a9210cc96c
ShaffyYoutube <thedave2cz@gmail.com>
2019-04-20 16:59:05 +0200

    Vegetal-Pull
    I will now do Rat Race

--- Graphics/RhythmGame/Veggie1/Veggie1.asm
+++ Graphics/RhythmGame/Veggie1/Veggie1.asm
@@ -2,13 +2,13 @@
 .dw bee62c_veggie1_intro |0x80000000
 //Title
 .org 0x088AF6B4
-.dw X8AF6A0_assembly0
+.dw XD45D4E_assembly0
 .org 0x088AF6BC
-.dw X8AF68C_assembly1
+.dw XD45D68_assembly1
 .org 0x088AF6C4
-.dw X8AF678_assembly2
+.dw XD45D82_assembly2
 .org 0x088AF6CC
-.dw X8AF664_assembly3
+.dw XD45D9C_assembly3
 .org 0x088AF6D4
-.dw X8AF650_assembly4
+.dw XD45DB6_assembly4
 ;eof
--- Graphics/RhythmGame/Veggie1/Veggie1_relocate.asm
+++ Graphics/RhythmGame/Veggie1/Veggie1_relocate.asm
@@ -3,18 +3,18 @@ bee62c_veggie1_intro:
 .import Graphics/RhythmGame/Veggie1/bee62c_veggie1_intro.cdat
 //Title
 .align 2
-X8AF6A0_assembly0:
-.import Graphics/RhythmGame/Veggie1/8AF6A0_assembly0.bin
+XD45D4E_assembly0:
+.import Graphics/RhythmGame/Veggie1/D45D4E_assembly0.bin
 .align 2
-X8AF68C_assembly1:
-.import Graphics/RhythmGame/Veggie1/8AF68C_assembly1.bin
+XD45D68_assembly1:
+.import Graphics/RhythmGame/Veggie1/D45D68_assembly1.bin
 .align 2
-X8AF678_assembly2:
-.import Graphics/RhythmGame/Veggie1/8AF678_assembly2.bin
+XD45D82_assembly2:
+.import Graphics/RhythmGame/Veggie1/D45D82_assembly2.bin
 .align 2
-X8AF664_assembly3:
-.import Graphics/RhythmGame/Veggie1/8AF664_assembly3.bin
+XD45D9C_assembly3:
+.import Graphics/RhythmGame/Veggie1/D45D9C_assembly3.bin
 .align 2
-X8AF650_assembly4:
-.import Graphics/RhythmGame/Veggie1/8AF650_assembly4.bin
+XD45DB6_assembly4:
+.import Graphics/RhythmGame/Veggie1/D45DB6_assembly4.bin
 ;eof
